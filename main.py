e1909d5d1712977c6aad41f48adebc924ad20a8c
achille1017 <achillemarseille@gmail.com>
2022-12-05 07:33:58 +0100

    issues 17:22

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from pytezos.crypto import key
 import logging
 logging.basicConfig(level=logging.INFO)
 issuer_key = json.dumps(json.load(open("keys.json", "r"))['talao_Ed25519_private_key'])
-issuer_did = "did:tz:tz1NyjrTUNxDpPaqNZ84ipGELAcTWYg6s5Du"
-issuer_vm = "did:tz:tz1NyjrTUNxDpPaqNZ84ipGELAcTWYg6s5Du#blockchainAccountId"
+issuer_vm = "did:web:app.altme.io:issuer#key-1"
+issuer_did = "did:web:app.altme.io:issuer"
 w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/"+json.dumps(json.load(open("keys.json", "r"))["infuraApiKey"])))
 
 def create_payload (input, type) :
