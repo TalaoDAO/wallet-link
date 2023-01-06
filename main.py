e102805e986f807fba7f3e668a5439151ba917b4
achille1017 <achillemarseille@gmail.com>
2023-01-06 10:57:24 +0100

    frontend

--- main.py
+++ main.py
@@ -332,9 +332,6 @@ def validate_sign():
             print(session.get('nonce'))
             address = w3.eth.account.recoverHash(message_hash, signature=request.headers.get('signature'))
             logging.info("address verified : " +address)
-
-            address2 = w3.eth.account.recoverHash(session.get('nonce'), signature=request.headers.get('signature'))
-            logging.info("address verified2 : " +address2)
             session["addressVerified"]=address
             return({'status':'ok'}),200
         except ValueError:
